62fc5d9911ba34bf646a34d6a984cb07444aae24
wqlzyy <blessyou@163.com>
2021-01-31 11:25:23 +0800

    User authentication with Flask-login

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
 import click
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import LoginManager,UserMixin,login_user
+from flask_login import login_required,logout_user,current_user
+
+
 
 WIN=sys.platform.startswith('win')
 if WIN:
@@ -17,10 +22,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY']='dev'
 
 db=SQLAlchemy(app)
+login_manager=LoginManager(app)
+login_manager.login_view='login'
 
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
         title = request.form['title']
         year = request.form['year']
 
@@ -38,9 +47,16 @@ def index():
     return render_template('index.html', movies=movies)
 
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(20))
+    username=db.Column(db.String(20))
+    password_hash=db.Column(db.String(128))
+
+    def set_password(self,password):
+        self.password_hash=generate_password_hash(password)
+    def validate_password(self,password):
+        return check_password_hash(self.password_hash,password)    
 
 class Movie(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -80,6 +96,23 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+@app.cli.command()
+@click.option('--username',prompt=True,help='The username used to login.')
+@click.option('--password',prompt=True,hide_input=True,confirmation_prompt=True,help='The password used to login.')
+def admin(username,password):
+    db.create_all()
+    user=User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.set_password(password)
+    else:
+        click.echo('Creating user...')
+        user=User(username=username,name='Admin')
+        user.set_password(password)
+        db.session.add(user)
+    db.session.commit()
+    click.echo('Done.')
+
 
 @app.context_processor
 def inject_user():
@@ -92,6 +125,7 @@ def page_not_found(e):
 
 
 @app.route('/movie/edit/<int:movie_id>',methods=['GET','POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -112,9 +146,53 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash('Item deleted.')
     return redirect(url_for('index'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    user=User.query.get(int(user_id))
+    return user
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        username=request.form['username']
+        password=request.form['password']
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+        user=User.query.first()
+        if username==user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success.')
+            return redirect(url_for('index'))
+        flash('Invalid username or password.')
+        return redirect(url_for('login'))
+    return render_template('login.html')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye.')
+    return redirect(url_for('index'))
+
+@app.route('/settings',methods=['GET','POST'])
+@login_required
+def settings():
+    if request.method=='POST':
+        name=request.form['name']
+
+        if not name or len(name)>20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+        current_user.name=name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+    return render_template('settings.html')
